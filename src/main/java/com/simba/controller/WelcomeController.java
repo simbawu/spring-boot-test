ffeab5bc4cb9cdc4527621c34255544519c4f5a7
wusiying <wusiying@anta.com>
2019-01-21 10:45:41 +0800

    Add log config.

--- src/main/java/com/simba/controller/WelcomeController.java
+++ src/main/java/com/simba/controller/WelcomeController.java
@@ -2,6 +2,8 @@ package com.simba.controller;
 
 import com.simba.domain.WelcomeRequest;
 import com.simba.domain.WelcomeResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +12,10 @@ import java.time.Instant;
 
 @RestController
 public class WelcomeController {
+    Logger logger = LoggerFactory.getLogger(WelcomeController.class);
     @RequestMapping("/welcome")
     public WelcomeResponse welcome(WelcomeRequest welcomeRequest){
+        logger.info("Welcome " + welcomeRequest.getName());
         WelcomeResponse response = new WelcomeResponse();
         response.setWebcomeWords("Welcome "  + welcomeRequest.getName() + " to Spring Boot World! ");
         response.setTime(Instant.now().toString());
--- src/test/java/com/simba/db/util/DBUtilTest.java
+++ src/test/java/com/simba/db/util/DBUtilTest.java
@@ -1,8 +1,11 @@
 package com.simba.db.util;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DBUtilTest {
+    Logger logger = LoggerFactory.getLogger(DBUtilTest.class.getName());
     @Test
     public void testInsertSQL() throws Exception {
         VosAreaView view = new VosAreaView();
@@ -11,5 +14,6 @@ public class DBUtilTest {
         view.setCompanyId("Anta");
         System.out.println(DBUtil.generateInsertSQL(view));
         System.out.println(DBUtil.generateUpdateSQL(view));
+        logger.info(DBUtil.generateUpdateSQL(view));
     }
 }
