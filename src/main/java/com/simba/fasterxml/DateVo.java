ae69665e1a296c0aa9c0781b825e83832158a08c
wusiying <wusiying@anta.com>
2019-11-28 17:57:49 +0800

    change db to db4free.

---
+++ src/main/java/com/simba/fasterxml/DateVo.java
@@ -0,0 +1,24 @@
+package com.simba.fasterxml;
+
+import java.util.Date;
+
+public class DateVo {
+    Date birth;
+    Date hireDate;
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public Date getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(Date hireDate) {
+        this.hireDate = hireDate;
+    }
+}
--- src/test/java/com/simba/fasterxml/FasterXmlTest.java
+++ src/test/java/com/simba/fasterxml/FasterXmlTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 //import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 //@RunWith(SpringRunner.class)
 //@SpringBootTest
@@ -25,5 +26,18 @@ public class FasterXmlTest {
             e.printStackTrace();
         }
 
+    }
+    @Test
+    public void readDate(){
+        String jsonString = "{\"birth\" : \"1985-02-01 09:00:00\", \"hireDate\" : \"1995-02-01\"}";
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+        //mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd"));
+        try {
+            DateVo json = mapper.readValue(jsonString, DateVo.class);
+            Assert.assertNotNull(json);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
