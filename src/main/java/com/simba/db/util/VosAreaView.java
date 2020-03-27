ea01404e3860d652b87bb6cca1113d6eaaeef817
simbawu <wsy85@sina.com>
2020-03-28 00:08:37 +0800

    Upgrade Spring boot to 2.2.5 for using jdk10 without warning. Change the directory structure for IDEA to understand the spring boot fact.

--- src/main/java/com/simba/db/util/VosAreaView.java
+++ src/main/java/com/simba/db/util/VosAreaView.java
@@ -5,7 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Entity
+//@Entity
 //@Table(name = "vos_area_view")
 public class VosAreaView {
 	@Column(name = "companyid")
